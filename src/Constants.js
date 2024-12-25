18b439ae85665e0e2c85f28f05504e81896c7d0c
yamz-11 <wiratamajrz@gmail.com>
2024-12-26 02:33:26 +0700

    up project

--- src/Constants.js
+++ src/Constants.js
@@ -7,7 +7,7 @@ import PROJECT6 from "./Assets/Projects/project6.jpg";
 import PROJECT7 from "./Assets/Projects/project7.jpg";
 import PROJECT8 from "./Assets/Projects/project8.jpg";
 import PROJECT9 from "./Assets/Projects/project9.jpg";
-// import PROJECT10 from "./Assets/Projects/project10.png";
+import PROJECT10 from "./Assets/Projects/project10.png";
 import PROJECT11 from "./Assets/Projects/project11.png";
 import PROJECT12 from "./Assets/Projects/project12.png";
 import PROJECT13 from "./Assets/Projects/project13.png";
@@ -16,63 +16,64 @@ export const PROJECTS = [
   {
     image: PROJECT13,
     name: "YamzStore",
-    technologyUsed: "Laravel filament, livewire, Tailwind, mysql",
-    description: [
-      "Web Store Premium Apps"
-    ],    
+    technologyUsed: "Laravel filament, Tailwind, mysql",
+    description: ["Web Store Premium Apps"],
     url: "https://athallariq11.github.io/YamzStore.github.io/",
+  },
+  {
+    image: PROJECT10,
+    name: "Simple Website Finance Record",
+    technologyUsed: "Laravel Filament",
+    description: [""],
+    url: "",
   },
   {
     image: PROJECT4,
     name: "PinjamSimple",
-    technologyUsed: "Laravel, Tailwind, Mysql",
+    technologyUsed: "Laravel,Tailwind, Mysql",
     description: [
       "PinjamSimple is a simple book borrowing website consisting of several features: Login, Register, Books, Category, Borrowing, members, mailtrap etc. ",
       "There are admin and user roles: ",
       "Admin can CRUD book pages and categories, make users admin, and delete users. ",
-      "User can Borrow and return books."
-    ],    
+      "User can Borrow and return books.",
+    ],
     url: "",
   },
   {
     image: PROJECT5,
     name: "SetakitHama",
     technologyUsed: "Laravel, Mysql",
-    description: [
-      "Pest management information website capstone project"
-    ],    
+    description: ["Pest management information website capstone project"],
     url: "",
   },
   {
     image: PROJECT3,
     name: "ToDoList",
     technologyUsed: "Laravel, mySql",
-    description: "Simple todolist has login, register, todo, todo category and member features. Next, for admins and users, admins can CRUD on the todo and todo category pages, delete members and make members admins",
+    description:
+      "Simple todolist has login, register, todo, todo category and member features. Next, for admins and users, admins can CRUD on the todo and todo category pages, delete members and make members admins",
     url: "",
   },
   {
     image: PROJECT11,
     name: "Sertifikasi Programmer",
     technologyUsed: "BNSP",
-    description: [
-      ""
-    ],    
+    description: [""],
     url: "https://drive.google.com/file/d/1HcM5HeHKFb1pUQb_US9lXSstZVZCXzv7/view?usp=sharing",
   },
   {
     image: PROJECT12,
     name: "Sertifikasi Software Deployment",
     technologyUsed: "",
-    description: [
-      ""
-    ],    
+    description: [""],
     url: "https://drive.google.com/file/d/1N0Hm2s8930VI-dTe8tROPG5MXnUeIE6q/view?usp=sharing",
   },
   {
     image: PROJECT2,
     name: "Tikbus",
     technologyUsed: "Java, Firebase",
-    description: "My final project is to create a bus ticket booking application using Android studio tools. Bus Ticket Booking Application, Consists of features: -Login and Register -Check ticket -availability Book tickets -Check Ticket Prices -Departure Report   ",
+    description:
+      "My final project is to create a bus ticket booking application using Android studio tools. Bus Ticket Booking Application, Consists of features: -Login and Register -Check ticket -availability Book tickets -Check Ticket Prices -Departure Report   ",
     url: "",
   },
   {
@@ -86,57 +87,40 @@ export const PROJECTS = [
     image: PROJECT8,
     name: "Simple Resfull API",
     technologyUsed: "Java SpringBoot, mySql",
-    description: [
-      ""
-    ],    
+    description: ["Assigment"],
     url: "",
   },
   {
     image: PROJECT7,
     name: "Toko Sayur",
     technologyUsed: "Java SpringBoot",
-    description: [
-      "Assigment"
-    ],    
+    description: ["Assigment"],
     url: "",
   },
   {
     image: PROJECT9,
     name: "Weight Convert",
     technologyUsed: "Java SpringBoot",
-    description: [
-      ""
-    ],    
+    description: ["Assigment"],
     url: "",
   },
   {
     image: PROJECT6,
     name: "Suhu Count",
     technologyUsed: "Java SpringBoot",
-    description: [
-      "Assigment"
-    ],    
+    description: ["Assigment"],
     url: "",
   },
 ];
 
-export const SKILLS = [
-  { name: "ReactJs", initialRating: 4 },
-  { name: "React-Native", initialRating: 4 },
-  { name: "JavaScript", initialRating: 4 },
-  { name: "Redux", initialRating: 4 },
-  { name: "Shopify/Reactjs", initialRating: 4 },
-  { name: "Nodejs", initialRating: 3 },
-  { name: "Koajs", initialRating: 3 },
-  { name: "Html", initialRating: 4 },
-  { name: "CSS", initialRating: 4 },
-  { name: "Unit-Testing(Jest)", initialRating: 3 },
-  { name: "TypeScript", initialRating: 4 },
-  { name: "Graphql", initialRating: 4 },
-  { name: "Apollo", initialRating: 4 },
-  { name: "AntD | ReactStrap | Material UI", initialRating: 4 },
-  { name: "Shopify Polaris", initialRating: 4 },
-  { name: "Git", initialRating: 4 },
+export const TOOLS = [
+  "Visual Studio Code",
+  "Git",
+  "Chrome DevTools",
+  "Npm (Node Package Manager)",
+  "Atom",
+  "Sublime Text",
+  "Linux",
+  "Heroku",
+  "Postman",
 ];
-
-export const TOOLS = ["Visual Studio Code", "Git", "Chrome DevTools", "Npm (Node Package Manager)", "Atom", "Sublime Text", "Linux", "Heroku", "Postman"]
